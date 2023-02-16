ed44acb9836fae2d4629d7ed603e2d8853981d76
AdenPieters <adenpieters21@gmail.com>
2023-02-16 06:25:25 -0800

    ALX

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - Print a string
+ * Description: use printf
+ * Return: 0
+ */
+
+int main(void)
+{
+	print("%s", "with proper grammer, but the outcome is a piece of art,\n");
+	retun(0);
+}
