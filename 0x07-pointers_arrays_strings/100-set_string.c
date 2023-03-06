a8a2d22b622850b69e62f3814d2fd30093896f6b
Aden Pieters <122808789+AdenPieters@users.noreply.github.com>
2023-03-06 16:17:06 +0200

    ALX

--- 0x07-pointers_arrays_strings/100-set_string.c
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -1,11 +1,12 @@
 #include "main.h"
 
 /**
- * set_string - Sets the value of a pointer to a char.
- * @s: The pointer.
- * @to: The char.
- */
+* set_string - sets the value of a pointer to a char
+* @s: source adress
+* @to: target adress
+* Return: no return
+*/
 void set_string(char **s, char *to)
 {
-	*s = to;
+*s = to;
 }
