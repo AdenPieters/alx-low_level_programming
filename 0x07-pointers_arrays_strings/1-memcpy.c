442c67c7b80ef016712d12807bf7f7fbbc7e0815
Aden Pieters <122808789+AdenPieters@users.noreply.github.com>
2023-03-06 16:16:08 +0200

    ALX

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -1,20 +1,19 @@
 #include "main.h"
-
 /**
- * *_memcpy - copies memory area
- * @dest: memory area
- * @src: source
- * @n: length of src
- *
- * Return: pointer to dest.
- */
+* _memcpy - Fills a buffer with a constant for a specified size
+* @dest: This is the buffer to be filled
+* @src: This is the char that will be used to fill buffer
+* @n: This is the no of bytes to fill in the memory array of dest
+* Return: returns dest
+*/
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
-	unsigned int i;
+unsigned int i;
 
-	for (i = 0; i < n; i++)
-	{
-		dest[i] = src[i];
-	}
-	return (dest);
+for (i = 0; i < n; i++)
+{
+dest[i] = src[i];
+}
+
+return (dest);
 }
